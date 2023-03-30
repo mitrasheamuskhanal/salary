cfb53fea76a54a06062c40f5bd29ed52fd6dc407
mitrasheamuskhanalgit config --global user.name mitrasheamuskhanalgit config --global user.name mitrasheamuskhanal <mitrasheamuskhanal@gmail.com>
2023-03-30 06:03:00 +0545

    making classes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 class employee
 {
+     public string Name{ get; set; }
+    public double salary { get; set; }
 
+    public virtual void getFullName()
+    {
+        Console.WriteLine("employee name:" + Name)
+    }
+    public virtual double getYearlySalary()
+    {
+        return salary;
+    }
 }
