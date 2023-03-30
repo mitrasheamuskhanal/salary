bd181eeaad5c1c32351efdc0a178703198b39338
mitrasheamuskhanalgit config --global user.name mitrasheamuskhanalgit config --global user.name mitrasheamuskhanal <mitrasheamuskhanal@gmail.com>
2023-03-30 06:19:22 +0545

    writing coe on main

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
 ﻿using System;
+class execute
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Your Full Name ? :");
+        string name = Console.ReadLine();
+        Console.Write("What is Your Salary");
+
+    }
+}
 class employee
 {
      public string Name{ get; set; }
@@ -6,10 +16,28 @@ class employee
 
     public virtual void getFullName()
     {
-        Console.WriteLine("employee name:" + Name)
+        Console.WriteLine("employee name:" + Name);
     }
     public virtual double getYearlySalary()
     {
         return salary;
     }
 }
+class FTE : employee
+{
+    public override double getYearlySalary()
+    {
+        return salary * 12;
+    }
+}
+class PTE : employee
+{
+  public double workedHour { get; set; }
+ public double hourlyRate { get; set;}
+
+    public override double getYearlySalary()
+    {
+        return workedHour * hourlyRate;
+    }
+}
+
