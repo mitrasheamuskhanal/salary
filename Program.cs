bbce8f22c9d1e620ecd6e66901f76d82e704cb68
mitrasheamuskhanalgit config --global user.name mitrasheamuskhanalgit config --global user.name mitrasheamuskhanal <mitrasheamuskhanal@gmail.com>
2023-03-30 06:55:47 +0545

    trying using else if

--- Program.cs
+++ Program.cs
@@ -3,16 +3,24 @@ class execute
 {
     static void Main(string[] args)
     {
-        Console.Write("Your Full Name ? :");
-        string name = Console.ReadLine();
-        Console.Write("What is Your Salary : ");
-        double fullTimeSalary = Convert.ToDouble(Console.ReadLine()) ;
-        FTE fullTimeEmployee = new FTE();
-        fullTimeEmployee.Name = name;
-        fullTimeEmployee.salary = fullTimeSalary;
-        fullTimeEmployee.getFullName();
-        Console.WriteLine("Yearly salary"+fullTimeEmployee.getYearlySalary());
-
+        Console.Write("Who are You FTE OR PTE ? :");
+        string uservalue = Console.ReadLine();
+        if (uservalue == "FTE")
+        {
+            Console.Write("Your Full Name ? :");
+            string name = Console.ReadLine();
+            Console.Write("What is Your Salary : ");
+            double fullTimeSalary = Convert.ToDouble(Console.ReadLine());
+            FTE fullTimeEmployee = new FTE();
+            fullTimeEmployee.Name = name;
+            fullTimeEmployee.salary = fullTimeSalary;
+            fullTimeEmployee.getFullName();
+            Console.WriteLine("Yearly salary" + fullTimeEmployee.getYearlySalary());
+        }
+        else if (uservalue == "PTE")
+        {
+            Console.WriteLine("Your Full name ");
+        }
 
 
 
